9f1b2cedeebd00faedbc7f99bba0262ed3993345
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2020-10-15 14:16:48 +0200

    Readability code
    This commit just split the code in functions to improve
    readability.
    
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

--- or.py
+++ or.py
@@ -3,10 +3,9 @@
 import numpy as np
 import math
 
-
-# sigmoid as a loss function
-def sigmoid(z):
-    return 1 / (1 + math.exp(-z))
+#####################################
+#   INITIALIZE DATA
+#####################################
 
 # Random initial values for wights
 W = np.array([0.1, 0.6])
@@ -22,19 +21,32 @@ b = 0
 Y = np.array([0, 1, 1, 1])
 Y = np.reshape(Y, (4, 1))
 
-# Verify shapes to do dot product correctly
-print(W.shape)
-print(X.shape)
+# DEBUG: Verify shapes to do dot product
+# correctly.
+#print(W.shape)
+#print(X.shape)
 
-Z = np.dot(W, X.T) + b
+# sigmoid as a loss function
+def sigmoid(z):
+    return 1 / (1 + math.exp(-z))
 
-# define vector of sigmoid function
-Yp = np.vectorize(sigmoid)
+def forward():
+    # Calculate Z
+    Z = np.dot(W, X.T) + b
 
-# Evaluate every element of Z in sigmoid function
-Yp = Yp(Z)
+    # define vector of sigmoid function
+    Yp = np.vectorize(sigmoid)
 
-print ("*** Predicted Y (Forward propagation) ***")
-print(Yp)
+    # Evaluate every element of Z in sigmoid function
+    Yp = Yp(Z)
+
+    print ("*** Predicted Y (Forward propagation) ***")
+    print(Yp)
+
+def main():
+    forward()
+
+if __name__ == '__main__':
+    main()
 
 

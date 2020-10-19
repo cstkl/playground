f33ed81bc8d0aa6cf5f4d02f10c21be28e253acc
Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>
2020-10-19 21:52:06 +0200

    Remove global variables
    This change is to do modularization easier.
    
    Signed-off-by: Mario Alfredo Carrillo Arevalo <mario.carrillo.arevalo@gmail.com>

--- or.py
+++ or.py
@@ -3,34 +3,11 @@
 import numpy as np
 import math
 
-#####################################
-#   INITIALIZE DATA
-#####################################
-
-# Random initial values for wights
-W = np.array([0.1, 0.6])
-W = np.reshape(W, (1, 2))
-
-X = np.array([
-            [0, 0],
-            [0, 1],
-            [1, 0],
-            [1, 1]])
-
-b = 0
-Y = np.array([0, 1, 1, 1])
-Y = np.reshape(Y, (4, 1))
-
-# DEBUG: Verify shapes to do dot product
-# correctly.
-#print(W.shape)
-#print(X.shape)
-
 # sigmoid as a loss function
 def sigmoid(z):
     return 1 / (1 + math.exp(-z))
 
-def forward():
+def forward(W, X, b):
     # Calculate Z
     Z = np.dot(W, X.T) + b
 
@@ -49,7 +26,21 @@ def cost(desired_Y, predicted_Y):
     return np.sum(square_sub) / (2 * desired_Y.size)
 
 def main():
-    Yp = forward()
+    # Random initial values for wights
+    W = np.array([0.1, 0.6])
+    W = np.reshape(W, (1, 2))
+
+    X = np.array([
+            [0, 0],
+            [0, 1],
+            [1, 0],
+            [1, 1]])
+
+    b = 0
+    Y = np.array([0, 1, 1, 1])
+    Y = np.reshape(Y, (4, 1))
+
+    Yp = forward(W, X, b)
     print("Forward propagation: ", Yp)
 
     c = cost(Y, Yp)
